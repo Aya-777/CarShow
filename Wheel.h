d7f14d1b76a36a6a609019f75aca9083c5d9779c
MHD-fawzy-sukkar <eng.fawzysukkar@gmail.com>
2026-01-13 17:46:49 +0300

    Add Wheel class

---
+++ Wheel.h
@@ -0,0 +1,20 @@
+﻿#ifndef WHEEL_H
+#define WHEEL_H
+
+#include <GL/freeglut.h>
+#include "Point.h"
+
+class Wheel {
+private:
+    float width;
+    float radius;
+
+public:
+    float spinAngle;
+    float steerAngle;
+    Wheel(float width = 0.15f, float radius = 0.45f);
+
+    void draw(Point p, bool canSteer);
+};
+
+#endif
