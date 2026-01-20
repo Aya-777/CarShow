7ab199a930ae5bed7b84b9e649bb3f0ee008ce3a
Aya yassien <yassienaya714@gmail.com>
2026-01-20 23:58:33 +0300

    add ground to the building and make the door slides

--- Building.cpp
+++ Building.cpp
@@ -8,17 +8,18 @@
 
 void Building::draw() {
 
-	Cuboid wall1(Point(-215, -2, -279), 150, 255, 15); // front wide
-	Cuboid wall12(Point(-215, -2, 278), 150, 455, 15); // front door side left
+	Cuboid wall1(Point(-215, 2, -279), 150, 255, 15); // front wide
+	Cuboid wall12(Point(-215, 2, 278), 150, 455, 15); // front door side left
 	Cuboid wall13(Point(-215, 89, -52), 60, 205, 15); // front door side right
-	Cuboid wall2(Point(200, -2, 50), 150, 914, 15); // back wide
-	Cuboid wall3(Point(-500, -2, 0), 150, 415 , 15); // right short
-	Cuboid wall4(Point(400, -2, 0), 150, 415, 15); // left short
+	Cuboid wall2(Point(200, 2, 50), 150, 914, 15); // back wide
+	Cuboid wall3(Point(-500, 2, 0), 150, 415 , 15); // right short
+	Cuboid wall4(Point(400, 2, 0), 150, 415, 15); // left short
 	Cuboid roof(Point(50, -208, -152), 430, 10, 915); // roof
-	Cuboid insideWall1(Point(87, -2, -50), 150, 10, 250); // insideWall1
-	Cuboid insideWall2(Point(87, -2, 140), 150, 10, 200); // insideWall2
-	Cuboid insideWall3(Point(87, -2, 320), 150, 10, 200); // insideWall3
-	Cuboid administrationBuilding(Point(-140, -2, 570), 100, 150, 150); // administration building
+	Cuboid ground(Point(50, -208, 0), 430, 1, 915); // ground
+	Cuboid insideWall1(Point(87, 2, -50), 150, 10, 250); // insideWall1
+	Cuboid insideWall2(Point(87, 2, 140), 150, 10, 200); // insideWall2
+	Cuboid insideWall3(Point(87, 2, 320), 150, 10, 200); // insideWall3
+	Cuboid administrationBuilding(Point(-140, 2, 570), 100, 150, 150); // administration building
 	
 	float repeatX1 = 400 / 150.0;
 	float repeatY = 150 / 150;
@@ -43,6 +44,14 @@ void Building::draw() {
 	roof.drawWithTexture(wallTex,repeatX1,repeatY);
 	glPopMatrix();
 
+	//ground
+	glPushMatrix();
+	glRotatef(90, 1, 0, 0);
+	glRotatef(90, 0, 0, 1);
+	ground.drawWithTexture(groundTex, 7, 40);
+	glPopMatrix();
+
+
 	// inside walls
 	glPushMatrix();
 	glColor3f(1.0, 1.0, 1.0);
@@ -60,24 +69,29 @@ void Building::draw() {
 	// Door
 	glPushMatrix();
 	glRotatef(90, 0, 1, 0);
-	door.openWindow();
-	door.drawMovingWindow(0.7, 0.7, 0.7);
+	leftdoor.openWindow();
+	rightdoor.openWindow();
+	leftdoor.drawMovingWindow(0.7, 0.7, 0.7,true);
+	rightdoor.drawMovingWindow(0.7, 0.7, 0.7, false);
 	glPopMatrix();
 }
 
 void Building::toggleDoor()
 {
-	door.isOpen = !door.isOpen;
+	leftdoor.isOpen = !leftdoor.isOpen;
+	rightdoor.isOpen = !rightdoor.isOpen;
 }
 
 Building::Building()
-	: door(Point(50, -2, -220), 10, 202, 92, 0, 0, 0, 0, 1)
+	: leftdoor(Point(0, -2, -215), 10, 99, 88, 0, 0, 0, 0, 1), 
+	rightdoor(Point(100, -2, -215), 10, 98, 88, 0, 0, 0, 0, 1)
 {
 
 }
 
-Building::Building(double x2, double y2, double z2, double w, double h, double l,Window door)
-: door(door){
+Building::Building(double x2, double y2, double z2, double w, double h, double l,Window leftdoor, Window rightdoor)
+: leftdoor(leftdoor),
+  rightdoor(rightdoor){
 	width = w;
 	height = h;
 	length = l;
